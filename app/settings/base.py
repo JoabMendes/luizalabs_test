8026750ce8dacc7f04c00f67205db6118ec39cda
Joabe Mendes <joab.mendes.r2d2@gmail.com>
2019-06-16 23:34:39 -0300

    Updated allowed hosts

--- app/settings/base.py
+++ app/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p(xy-cg_e2=p94rxp7kjkx*wy3rc$@#(zqx3x_-5n*e-a&o+$2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'luizalabs-employe-manager.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'luizalabs-manager.herokuapp.com']
 
 # Application definition
 
